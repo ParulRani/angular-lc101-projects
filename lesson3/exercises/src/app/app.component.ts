918b315cdb431328c6fadcabae90e7b28d063420
Parul Rani <rathiparul90@gmail.com>
2021-04-24 17:36:54 -0500

    changes as per the exercise 3

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -12,4 +12,22 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+  takeOffEnabled: boolean = true;
+}
+function handleTakeOff() {
+  let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
+  if (result) {
+    this.color = 'blue';
+    this.height = 10000;
+    this.width = 0;
+    this.message = 'Shuttle in flight.';
+  }
+}
+
+function moveRocket(rocketImage, direction) {
+  if (direction === 'right') {
+    let movement = parseInt(rocketImage.style.left) + 10 + 'px';
+    rocketImage.style.left = movement;
+    this.width = this.width + 10000;
+  }
 }
